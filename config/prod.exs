0206d8dda4d6ffd56571e473621b403d0fb86969
C. R. Oldham <cro@ncbt.org>
2019-04-07 19:46:47 -0600

    Change demoweb to demo

--- config/prod.exs
+++ config/prod.exs
@@ -68,7 +68,7 @@ url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
 
-config :demo, DemoWeb.Repo,
+config :demo, Demo.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
